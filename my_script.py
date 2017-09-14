f2fd62a4b8430addfda305cf6a1e83d6108a3b82
Dianoa <diana.masher@gmail.com>
2017-09-14 10:08:10 -0500

    Agregó instrucción de abrir shape

--- my_script.py
+++ my_script.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 
 DIRECTORIO_SHAPE = '/media/dianvla/Libelle/Curso_Python/destdv1gw_c'
+mx_boundary = gpd.GeoDataFrame.from_file(DIRECTORIO_SHAPE)
 
-print(DIRECTORIO_SHAPE)
+print(mx_boundary.__class__)
